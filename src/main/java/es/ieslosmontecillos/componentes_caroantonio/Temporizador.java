cff77b38583403bd244f027ab1509b740a3bd96e
Antonio Caro <antoniojosecaropozo@gmail.com>
2025-02-17 22:47:35 +0100

    FEAT: javadoc Temporizador

--- src/main/java/es/ieslosmontecillos/componentes_caroantonio/Temporizador.java
+++ src/main/java/es/ieslosmontecillos/componentes_caroantonio/Temporizador.java
@@ -12,7 +12,9 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Label;
 import javafx.util.Duration;
-
+/**
+ * Timer component. This component extends from a label, and it's modified in order to act as a timer.
+ * **/
 public class Temporizador extends Label {
 
     private final Timeline timeline;
@@ -21,6 +23,9 @@ public class Temporizador extends Label {
     private final ObjectProperty<EventHandler<ActionEvent>> onFinished = new SimpleObjectProperty<>();
 
 
+    /**
+     * Constructor of the timer, it's purpose is to instance the timer, duh
+     * **/
     public Temporizador() {
         super();
 
@@ -51,6 +56,9 @@ public class Temporizador extends Label {
 
     }
 
+    /**
+     * This method starts the timer.
+     * **/
     public void play(){
 
         timeline.getKeyFrames().clear();
@@ -60,38 +68,62 @@ public class Temporizador extends Label {
     }
 
 
+    /**
+     * This method Stops the timer.
+     * **/
     public void stop(){
 
         timeline.stop();
     }
 
 
+    /**
+     * this method allows to get the timeLine field of the timer.
+     * **/
     public Timeline getTimeline() {
         return timeline;
     }
 
 
+    /**
+     * this method allows to set the time of the timer. The value must be set on seconds
+     * **/
     public void setTimer(int time) {
         timer.set(time);
     }
 
+    /**
+     *This method allows to get the time set on the timer at the moment of execution.
+     **/
     public int getTimer() {
         return timer.get();
     }
 
+    /**
+     * This method allows to get the time property.
+     **/
     public IntegerProperty timerProperty() {
         return timer;
     }
 
 
+    /**
+     * This method is used in order to get the EventHandler of the timer.
+     **/
     public EventHandler<ActionEvent> getOnFinished() {
         return onFinished.get();
     }
 
+    /**
+     * This method is used in order to get the OnFinishedProperty of the timer
+     **/
     public ObjectProperty<EventHandler<ActionEvent>> onFinishedProperty() {
         return onFinished;
     }
 
+    /**
+     * This method is used in order to set the action that will occur just after the timer ends up
+     **/
     public void setOnFinished(EventHandler<ActionEvent> onFinished) {
         this.onFinished.set(onFinished);
     }
